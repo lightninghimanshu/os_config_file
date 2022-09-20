88b04c12e9e66e4a27a5700bd57cd9432a64e2c8
Himanshu Sachdeva <lightninghimanshu86@gmail.com>
2022-09-20 19:41:34 +0530

    dd

--- q2.s
+++ q2.s
@@ -1,14 +1,32 @@
-extern printf
-extern scanf
 global main
+extern printf , scanf
 
 section .text
 main :
-    push msg
-    call printf
-    ret
-section .data
+    push rbp
+    mov rbp , rsp
+    sub rsp , 16
 
-msg :db "Hello world !" , 10 , 0
-input: db "%d" , 0
+    xor eax , eax
+    lea rdi , [ msg ] ; first parameter
+    call printf
     
+    mov eax , 0
+    lea rdi , [ format ]
+    lea rsi , [ number ]
+    call scanf
+
+    xor eax , eax
+    lea rdi , [ number ] ; first parameter
+    call printf
+
+    add rsp , 16
+    leave
+    ret
+
+section .data
+   msg : db " Enter a number : " , 0
+   msg2 : db " Looping % d of % d " , 10,0
+   format : db " % d " , 0
+section .bss
+   number resb 4
