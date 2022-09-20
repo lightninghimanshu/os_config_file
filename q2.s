e1a4fc0fa9916ba04d80e2842c8430f183807711
Himanshu Sachdeva <lightninghimanshu86@gmail.com>
2022-09-20 19:53:51 +0530

    yep

--- q2.s
+++ q2.s
@@ -8,17 +8,17 @@ main :
     sub rsp , 16
 
     xor eax , eax
-    lea rdi , [ msg ] ; first parameter
+    lea rdi , [msg] ; first parameter
     call printf
     
     mov eax , 0
-    lea rdi , [ format ]
-    lea rsi , [ number ]
+    lea rdi , [format]
+    lea rsi , [number]
     call scanf
 
     xor eax , eax
-    lea rdi , [ format]
-    mov edx , [ number ] ; first parameter
+    lea rdi , [format]
+    mov edx , [number] ; first parameter
     call printf
 
     add rsp , 16
@@ -26,7 +26,7 @@ main :
     ret
 
 section .data
-   msg : db " Enter a number : " , 0
-   format : db " %d " , 0
+   msg : db " Enter a number : ", 0
+   format : db "%d", 0
 section .bss
    number resb 4
