27395620e8fb4916b86febf1256a9d07da31b741
Himanshu Sachdeva <lightninghimanshu86@gmail.com>
2022-09-20 21:32:50 +0530

    saas

--- q2.s
+++ q2.s
@@ -40,11 +40,11 @@ main:
 
 
 section .data
-   enter_promt : db " Enter: ", 0, 10
-   input_int : db "%d", 0, 10
-   input_str : dd "%s",0, 10
-   output_int : db "%d", 0
-   output_str : db "%s", 0
+   enter_promt : db " Enter: ", 0
+   input_int : db "%d", 0
+   input_str : dd "%s",0
+   output_int : db "%d", 0, 10
+   output_str : db "%s", 0, 10
    string times 100 db 0
    
 section .bss
