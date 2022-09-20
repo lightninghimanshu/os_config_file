2fc31510ded9141742a7e305366ace16e53930ce
Himanshu Sachdeva <lightninghimanshu86@gmail.com>
2022-09-20 19:58:52 +0530

    sasa

--- q2.s
+++ q2.s
@@ -12,13 +12,27 @@ main :
     call printf
     
     mov eax , 0
-    lea rdi , [format]
+    lea rdi , [formatn]
     lea rsi , [number]
     call scanf
 
     xor eax , eax
-    lea rdi , [format]
+    lea rdi , [formatn]
     mov edx , [number] ; first parameter
+    call printf
+
+    xor eax , eax
+    lea rdi , [msg] ; first parameter
+    call printf    
+
+    mov eax , 0
+    lea rdi , [formats]
+    lea rsi , [string]
+    call scanf
+
+    xor eax , eax
+    lea rdi , [formats]
+    mov edx , [string] ; first parameter
     call printf
 
     add rsp , 16
@@ -27,6 +41,8 @@ main :
 
 section .data
    msg : db " Enter a number : ", 0
-   format : db "%d", 0
+   formatn : db "%d", 0
+   formats : db "%s", 0
 section .bss
    number resb 4
+   string resw 4
