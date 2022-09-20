2592ec4c85becd23503f9d58381b98ef854796c7
Himanshu Sachdeva <lightninghimanshu86@gmail.com>
2022-09-20 21:18:01 +0530

    ds

--- q2.s
+++ q2.s
@@ -42,8 +42,8 @@ main:
 section .data
    msg : db " Enter a number : ", 0
    msg2 : db " Enter a string : ", 0
-   formatn : db "%d",0
-   formats : db "%s",10,0
+   formatn : db "%d", 0
+   formats : dd "%s",10,0
    string times 100 db 0
    
 section .bss
