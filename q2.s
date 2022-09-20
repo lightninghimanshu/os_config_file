7e8eabc82a382d3c60ed94f80f56844568e9f0af
Himanshu Sachdeva <lightninghimanshu86@gmail.com>
2022-09-20 22:55:58 +0530

    ewqew

--- q2.s
+++ q2.s
@@ -1,10 +1,10 @@
 section .data
-   msg : db "Enter a number :", 0
-   msg2 : db "Enter a string :", 0
-   formatni : db "%d", 0
-   formatno : db "%d", 10, 0
-   formatsi : db "%s",0
-   formatso : db "%s",10,0
+   int_message : db "Enter a number :", 0
+   string_message : db "Enter a string :", 0
+   format_num_in : db "%d", 0
+   format_num_out : db "%d", 10, 0
+   format_str_in : db "%s",0
+   format_str_out : db "%s",10,0
    string times 100 db 0   
 section .bss
    number resb 16
@@ -18,30 +18,30 @@ main:
     sub rsp , 96
 
     xor eax , eax
-    lea rdi , [msg] 
+    lea rdi , [int_message] 
     call printf
     
     mov eax , 0
-    lea rdi , [formatni]
+    lea rdi , [format_num_in]
     lea rsi , [number]
     call scanf
 
     xor eax , eax
-    lea rdi , [formatno]
+    lea rdi , [format_num_out]
     mov edx , [number] 
     call printf
 
     xor eax , eax
-    lea rdi , [msg2] 
+    lea rdi , [string_message] 
     call printf    
 
     mov eax , 0
-    lea rdi , [formatsi]
+    lea rdi , [format_str_in]
     lea rsi , [string]
     call scanf
 
     xor eax , eax
-    lea rdi , [formatso]
+    lea rdi , [format_str_out]
     lea rsi , [string] 
     call printf
 
